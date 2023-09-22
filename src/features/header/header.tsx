fcfffbde605617a22e02961d969094c854519a67
GunterCW <gunterhall2019@gmail.com>
2023-09-22 20:07:03 +0400

    feat: add header

---
+++ src/features/header/header.tsx
@@ -0,0 +1,9 @@
+import { Box, Heading, Section, Text } from "@radix-ui/themes";
+
+export const Header = () => {
+  return (
+    <Section style={{ backgroundColor: "orange" }} size="1">
+      <Heading ml="4">Hacker News Fake</Heading>
+    </Section>
+  );
+};
