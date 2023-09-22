33bd6e2a7ae4f22ae07d077b2c10e6b4a5348a20
GunterCW <gunterhall2019@gmail.com>
2023-09-22 20:12:14 +0400

    style: add radix theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { Header } from "@/features/header/header";
+import { Theme } from "@radix-ui/themes";
+
 export default function Home() {
   return (
     <>
-      <div> 1</div>
+      <Theme
+        appearance="dark"
+        accentColor="crimson"
+        grayColor="sand"
+        panelBackground="solid"
+        scaling="90%"
+      >
+        <Header />
+      </Theme>
     </>
   );
 }
