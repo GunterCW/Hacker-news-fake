7800977616cd710cf7bf0915eff554c11bd06d80
GunterCW <gunterhall2019@gmail.com>
2023-09-22 20:52:58 +0400

    style: change header color

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -2,7 +2,7 @@ import { Box, Heading, Section, Text } from "@radix-ui/themes";
 
 export const Header = () => {
   return (
-    <Section style={{ backgroundColor: "orange" }} size="1">
+    <Section style={{ backgroundColor: "var(--pink-a9)" }} size="2">
       <Heading ml="4">Hacker News Fake</Heading>
     </Section>
   );
