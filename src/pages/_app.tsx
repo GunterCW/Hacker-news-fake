97335d02e08a16b4982f414470b00b6431ea9fcd
GunterCW <gunterhall2019@gmail.com>
2023-09-22 20:11:41 +0400

    style: add style reset and font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from "next/app";
+import "@radix-ui/themes/styles.css";
+import "@/styles/themes-config.css";
+import "@/styles/reset-styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
