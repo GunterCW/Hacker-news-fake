a92aebd17358b1cfb0b57f35bf0b132f29940890
GunterCW <gunterhall2019@gmail.com>
2023-09-24 19:58:26 +0400

    refactor: add ts intrerfaces and moved logic to news-list-page

--- src/features/news-list/news-list.tsx
+++ src/features/news-list/news-list.tsx
@@ -6,76 +6,83 @@ import {
 } from "@radix-ui/react-icons";
 import { Box, Flex, Grid, Section, Text } from "@radix-ui/themes";
 import React from "react";
-import axios from "axios";
 import { TimeAgo } from "@/shared/time-ago";
 
-export const NewsList = () => {
-  const [newsListOnLoad, setNewsListOnLoad] = React.useState<any>([]);
-  const getRequestOnLoad = () => {
-    axios
-      .get("http://hn.algolia.com/api/v1/search_by_date?tags=story")
-      .then(function (response: any) {
-        // обработка успешного запроса
-        setNewsListOnLoad(response.data.hits);
-      })
-      .catch(function (error: string) {
-        // обработка ошибки
-        console.log(error);
-      });
-  };
+interface OneNews {
+  points: string;
+  title: string;
+  author: string;
+  objectID: number;
+  url: string;
+  created_at: string;
+  num_comments: number;
+}
 
-  React.useEffect(() => {
-    getRequestOnLoad();
-  }, []);
+interface NewsListProps {
+  inputValue: string;
+  newsList: [];
+  getRequest: () => void;
+}
+
+export const NewsList = (props: NewsListProps) => {
+  const { inputValue, newsList, getRequest } = props;
+
+  console.log(inputValue);
 
   const createUserLink = (author: string) => {
-    const href = "https://news.ycombinator.com/user?id=";
-    return href + author;
+    return "https://news.ycombinator.com/user?id=" + author;
   };
 
   const createCommentaryForkLink = (forkId: number) => {
-    const href = "https://news.ycombinator.com/item?id=";
-    return href + forkId;
+    return "https://news.ycombinator.com/item?id=" + forkId;
   };
 
   const createCroppedURL = (baseURL: string, forkId: number) => {
     let url = new URL(baseURL || createCommentaryForkLink(forkId));
     if (url.host.length > 15) {
-      return url.host.substr(0, 15) + "...";
+      return url.host.substring(0, 15) + "...";
     }
     return url.host;
   };
 
-  console.log(TimeAgo(new Date("2023-09-24T10:30:24.000Z")));
-
-  return newsListOnLoad.map((oneNews: any, i: number) => {
+  return newsList.map((oneNews: OneNews, i: number) => {
     return (
       <Box key={i}>
         <Section size="1">
           <Grid columns="12" rows="1">
             <Box ml="3" style={{ gridColumn: "span 1" }}>
+              {/* news rating */}
               <Text>{oneNews.points}</Text>
             </Box>
             <Box mr="9" style={{ gridColumn: "span 4" }}>
+              {/* news title */}
               <Text>{oneNews.title}</Text>
             </Box>
             <Flex mr="9" align="center" style={{ gridColumn: "span 2" }}>
+              {/* author name */}
               <PersonIcon color="red" />
               <a href={createUserLink(oneNews.author)}>{oneNews.author}</a>
             </Flex>
             <Flex mr="9" align="center" style={{ gridColumn: "span 2" }}>
+              {/* site */}
               <GlobeIcon />
               <Box ml="1">
-                <a href={createCommentaryForkLink(oneNews.objectID)}>
+                <a
+                  href={
+                    oneNews.url || createCommentaryForkLink(oneNews.objectID)
+                  }
+                >
                   {createCroppedURL(oneNews.url, oneNews.objectID)}
                 </a>
               </Box>
             </Flex>
             <Flex mr="9" align="center" style={{ gridColumn: "span 2" }}>
+              {/* date */}
               <ClockIcon />
               <Box ml="1">{TimeAgo(new Date(oneNews.created_at))}</Box>
             </Flex>
             <Flex mr="9" align="center" style={{ gridColumn: "span 1" }}>
+              {/* comments number */}
               <ChatBubbleIcon />
               <Box ml="1">
                 <a href={createCommentaryForkLink(oneNews.objectID)}>
