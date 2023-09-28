b50332303163488d9b8d470b0c224342e9f25c65
GunterCW <gunterhall2019@gmail.com>
2023-09-28 14:09:27 +0400

    refactor: change theme location

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,18 @@ import type { AppProps } from "next/app";
 import "@radix-ui/themes/styles.css";
 import "@/styles/themes-config.css";
 import "@/styles/reset-styles.css";
+import { Theme } from "@radix-ui/themes";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Theme
+      appearance="dark"
+      accentColor="crimson"
+      grayColor="sand"
+      panelBackground="solid"
+      scaling="90%"
+    >
+      <Component {...pageProps} />
+    </Theme>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,11 @@
 import { Header } from "@/features/header/header";
 import { NewsListPage } from "@/features/news-list-page/news-list-page";
-import { Theme } from "@radix-ui/themes";
 
 export default function Home() {
   return (
     <>
-      <Theme
-        appearance="dark"
-        accentColor="crimson"
-        grayColor="sand"
-        panelBackground="solid"
-        scaling="90%"
-      >
-        <Header />
-        <NewsListPage />
-      </Theme>
+      <Header />
+      <NewsListPage />
     </>
   );
 }
